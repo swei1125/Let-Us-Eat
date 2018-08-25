d2378212e262f4bb5ca861b3766d8fc407ab5887
Natasha <wsyalways2@gmail.com>
2018-08-25 16:06:01 -0700

    add bodyparser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const db = require('./config/keys').mongoURI;
 mongoose
@@ -12,3 +13,5 @@ app.get("/", (req, res) => res.send("Hello"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
