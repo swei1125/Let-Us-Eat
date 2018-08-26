f609d69cab50beb74d263947b7aa8fd2de3f3a87
Jose Martinez <josmmartinez94@gmail.com>
2018-08-25 17:37:28 -0700

    Add user model and user route

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const users = require("./routes/api/users");
+
 const db = require('./config/keys').mongoURI;
 mongoose
     .connect(db)
@@ -15,3 +17,4 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/api/users", users);
