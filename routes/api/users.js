831bb336c46a86392d8c816cfc2b2d30e71459da
Jose Martinez <josmmartinez94@gmail.com>
2018-08-25 18:38:57 -0700

    Add login functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -21,7 +21,7 @@ router.post('/register', (req, res) => {
                 })
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if (err) throw err;
+                        // if (err) throw err;
                         newUser.password = hash;
                         newUser.save()
                             .then(user => res.json(user))
@@ -30,6 +30,27 @@ router.post('/register', (req, res) => {
                 })  
             }
         })
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+    .then(user => {
+        if (!user) {
+            return res.status(404).json({ email: 'User does not exist'})
+        }
+
+        bcrypt.compare(password, user.password)
+        .then(isMatch => {
+            if (isMatch) {
+                res.json({ msg: 'Success' });
+            } else {
+                return res.status(400).json({ password: 'Invalid username and/or password'})
+            }
+        })
+    })
 })
 
 
