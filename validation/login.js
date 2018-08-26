8311a5d6ff5f32f57d6b1d725863d000f2555685
Jose Martinez <josmmartinez94@gmail.com>
2018-08-26 11:24:51 -0700

    Add validation fields in users.js route

--- validation/login.js
+++ validation/login.js
@@ -1,5 +1,5 @@
 const Validator = require('validator');
-const isEmpty = requre('./is-empty');
+const isEmpty = require('./is-empty');
 
 module.exports = function validateLoginInput(data) {
     let errors = {}
