122d24aa5127fffbe1cf1388f280d3848e019499
Nataly Menares <nataly.menares@gmail.com>
2018-08-27 10:53:49 -0700

    Add search component

---
+++ frontend/app/components/search/search.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+// import { withRouter } from 'react-router-dom';
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state: {
+            term: "restaurants",
+            location: "",
+            radius: 25,
+            price: ""
+        }
+    }
+
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        });
+    }
+
+    handleSubmit() {
+        this.props.fetchrestaurants(this.state);
+    }
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit.bind(this)>
+
+                    <label>Where?
+                        <input type="text" onChange={this.update('zip')} value={this.state.zip} />
+                    </label>
+
+                    <label>Max. Distance
+                        <input type="number" onChange={this.update('radius')} value={this.state.radius} />
+                    </label>
+
+                    <label>Price
+                        <input type="text" onChange={this.update('price')} value={this.state.price} />
+                    </label>
+                    
+                    <input type="submit" value="search" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search;
