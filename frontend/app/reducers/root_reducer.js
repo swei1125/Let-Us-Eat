802b805499b5c1f13b6842518191ab4eaca4b8cb
Jose Martinez <josmmartinez94@gmail.com>
2018-08-28 13:59:28 -0700

    Create session_form.jsx with signup_form_container.jsx, start form

--- frontend/app/reducers/root_reducer.js
+++ frontend/app/reducers/root_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import entitiesReducer from './entities_reducer';
+import errors from './session_errors_reducer';
+import session from './session_reducer';
 
 const rootReducer = combineReducers({
     entities: entitiesReducer,
+    errors,
+    session
 });
 
 export default rootReducer;
--- frontend/app/util/session_api_util.js
+++ frontend/app/util/session_api_util.js
@@ -1,47 +1,84 @@
-// import $ from 'jquery';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 
 const $ = window.$;
-export const GET_ERRORS = 'GET_ERRORS'
+export const GET_ERRORS = 'GET_ERRORS';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
-export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 
+// We can use axios to set a default header
 export const setAuthToken = token => {
     if (token) {
-        axios.defaults.headers.common['Authorization'] = token
+        // Apply to every request
+        axios.defaults.headers.common['Authorization'] = token;
     } else {
+        // Delete auth header
         delete axios.defaults.headers.common['Authorization'];
     }
-}
+};
 
+// Register User
 export const registerUser = (userData, history) => dispatch => {
     axios
-    .post('api/users/login', userData)
-    .then(res => {
-        const { token } = res.data
-        localStorage.setItem('jwtToken', token)
-        setAuthToken(token)
-        const decoded = jwt_decode(token)
-        dispatch(setCurrentUser(decoded))
-    })
-    .catch(err => 
-        dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
+        .post('/api/users/register', userData)
+        .then(res => {
+            // Save to localStorage
+            const { token } = res.data;
+            // Set token to ls
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth header
+            setAuthToken(token);
+            // Decode token to get user data
+            const decoded = jwt_decode(token);
+            // Set current user
+            dispatch(setCurrentUser(decoded));
         })
-    )
-}   
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
 
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+    axios
+        .post('/api/users/login', userData)
+        .then(res => {
+            // Save to localStorage
+            const { token } = res.data;
+            // Set token to ls
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth header
+            setAuthToken(token);
+            // Decode token to get user data
+            const decoded = jwt_decode(token);
+            // Set current user
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
+
+// Set logged in user
 export const setCurrentUser = decoded => {
     return {
         type: RECEIVE_CURRENT_USER,
         payload: decoded
-    }
-}
+    };
+};
 
+// Log user out
 export const logoutUser = () => dispatch => {
-    localStorage.removeItem('jwtToken')
+    // Remove token from localStorage
+    localStorage.removeItem('jwtToken');
+    // Remove auth header for future requests
     setAuthToken(false);
-    dispatch(setCurrentUser({}))
-}
+    // Set current user to {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
+};
