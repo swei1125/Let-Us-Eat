9f24dd872b589d9c0c515c9aef9ea80c47bb1a3b
Jose Martinez <josmmartinez94@gmail.com>
2018-08-31 09:47:54 -0700

    Fix typo on production_vars.js

--- config/production_vars.js
+++ config/production_vars.js
@@ -2,7 +2,7 @@ let keys = {}
 
 if (process.env.IS_PRODUCTION) {
     keys.mongoURI = process.env.MONGO_DB
-    keys.secretOrKey = process.env.SECRECT_OR-KEY
+    keys.secretOrKey = process.env.SECRECT_OR_KEY
     keys.mapKey = process.env.MAP_KEY
     keys.yelpKey = process.env.YELP_KEY   
 } else {
